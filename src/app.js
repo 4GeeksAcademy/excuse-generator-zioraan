f13e93778d5db87e954979f07cfbebeb2a85c3dd
Zioraan <167449198+Zioraan@users.noreply.github.com>
2024-06-04 01:23:39 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let entity = [
+  " My dog",
+  "My cat",
+  "A Bear",
+  "A squirrel",
+  "The mailman",
+  "My Grandma",
+  "A tiger",
+  "A mountain lion",
+  "Ryan Reynolds",
+  "My girlfriend"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const chosenEntity = chooseRandomItem(entity);
+
+let action = [
+  "destroyed",
+  "kicked",
+  "punched",
+  "stalked",
+  "slapped",
+  "smacked",
+  "burned",
+  "incinerated",
+  "broke",
+  "ate",
+  "bit"
+];
+
+const chosenAction = chooseRandomItem(action);
+
+let object = [
+  "my house",
+  "my apartment",
+  "my car",
+  "my dog",
+  "my cat",
+  "my dishwasher",
+  "my grandpa",
+  "my brother",
+  "my mom"
+];
+
+const chosenObject = chooseRandomItem(object);
+
+const newHeader = `I'm sorry I'm late but what happened was, ${chosenEntity} ${chosenAction} ${chosenObject}.`;
+
+const h1 = document.querySelector("h1");
+
+h1.textContent = newHeader;
+
+function chooseRandomItem(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
